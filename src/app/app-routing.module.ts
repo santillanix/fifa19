4884d3a39c6df46984201829735aadb9916bac8b
Santi <snunez@mariabarcelona.com>
2018-11-23 14:58:03 +0100

    al lio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewReglaComponent } from './components/new-regla/new-regla.component';
+import { IndexComponent } from './components/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'new',
+    component: NewReglaComponent
+  },
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/index'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
