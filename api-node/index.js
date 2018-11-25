7349beda40bd6583dca7803e18cbfc155b5f9f19
snunez <snunez@mariabarcelona.com>
2018-11-25 18:49:15 +0100

    cambios

---
+++ api-node/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 4100
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+app.listen(port, function () {
+    console.log(`Server Funcionando por el puerto ${port}`)
+})
